0c144d2bba94e58ecd05b52b9188db4e7a12307c
Oran Fry <oran@emerg.co>
2023-04-25 18:19:31 +1200

    Config
    Remove http-base.

--- config.php
+++ config.php
@@ -5,8 +5,7 @@ return (object) [
     'requires' => [
         APP_HOME . '/vendor/oranfry/jars-admin',
         APP_HOME . '/vendor/oranfry/jars-cli',
-        APP_HOME . '/vendor/oranfry/jars-http',
-        APP_HOME . '/vendor/oranfry/jars-http-base',
+        APP_HOME . '/vendor/oranfry/jars-http'
     ],
     'router' => 'jars\JarsRouter',
 ];

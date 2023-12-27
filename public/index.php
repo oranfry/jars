a8b8443e300b3fb81d8d48643aac57b413608476
Oran Fry <oran@emerg.co>
2023-12-27 15:38:33 +1300

    Portal Loading
    Allow reading portal information from etc dir, to save having to define the same config in multiple places.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ define('APP_HOME', dirname(__DIR__));
 
 @include APP_HOME . '/machine.php';
 
+$etc = defined('ETC_DIR') ? ETC_DIR : '/etc/jars';
 $host_config = (object) [];
 
 if (isset($_SERVER['HTTP_HOST']) && file_exists($config_file = __DIR__ . '/configs/' . $_SERVER['HTTP_HOST'] . '.php')) {
@@ -13,6 +14,14 @@ if (isset($_SERVER['HTTP_HOST']) && file_exists($config_file = __DIR__ . '/confi
         error_log('Jars Admin: Host config file should return an object.');
         die(1);
     }
+}
+
+if (
+    ($portal = $_SERVER['PORTAL'] ?? $host_config->portal ?? null)
+    && file_exists($config_file = $etc . '/' . $portal . '.json')
+    && ($portal_config = json_decode(file_get_contents($config_file)))
+) {
+    $host_config = (object) ((array) $host_config + (array) $portal_config);
 }
 
 if (!$connection_string = $host_config->connection_string ?? $_SERVER['CONNECTION_STRING'] ?? null) {

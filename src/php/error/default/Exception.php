be806936adcfecdebc06f8b263fca6d39ae191c7
Oran Fry <oran@emerg.co>
2025-03-12 11:16:00 +1300

    Sapi Detection
    Don't try to use a jars constant that is possibly not set.

--- src/php/error/default/Exception.php
+++ src/php/error/default/Exception.php
@@ -4,7 +4,7 @@ if (defined('SENTRY_DSN')) {
     if (
         defined('JARS_LOG_404S')
         && JARS_LOG_404S
-        || SUBSIMPLE_METHOD !== 'CLI'
+        || PHP_SAPI !== 'cli'
         && !$exception instanceof \subsimple\NotFoundException
     ) {
         \Sentry\captureException($exception);

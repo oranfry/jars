6d04668a1c8742a4d77fe25aebfed36a728df8bb
Oran Fry <oran@emerg.co>
2025-03-12 11:11:05 +1300

    Sentry Logging
    Suppress logs for not found exceptions on the web by default, as hackers may create large numbers of meaningless events.

--- src/php/error/default/Exception.php
+++ src/php/error/default/Exception.php
@@ -1,7 +1,14 @@
 <?php
 
 if (defined('SENTRY_DSN')) {
-    \Sentry\captureException($exception);
+    if (
+        defined('JARS_LOG_404S')
+        && JARS_LOG_404S
+        || SUBSIMPLE_METHOD !== 'CLI'
+        && !$exception instanceof \subsimple\NotFoundException
+    ) {
+        \Sentry\captureException($exception);
+    }
 }
 
 return false;
